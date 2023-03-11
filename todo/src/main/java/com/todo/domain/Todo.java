fa3f2201621cf82a912f78c9a2df79ca9e57844d
Tiago <tiago.polemarxgroup@gmail.com>
2023-03-11 13:12:39 -0300

    Classe de dominio criado

---
+++ todo/src/main/java/com/todo/domain/Todo.java
@@ -0,0 +1,102 @@
+package com.todo.domain;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_todo")
+public class Todo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dataParaFinalizar;
+	private Boolean finalizado = false;
+
+	public Todo() {
+
+	}
+
+	public Todo(Long id, String titulo, String descricao, LocalDateTime dataParaFinalizar, Boolean finalizado) {
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dataParaFinalizar = dataParaFinalizar;
+		this.finalizado = finalizado;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDataParaFinalizar() {
+		return dataParaFinalizar;
+	}
+
+	public void setDataParaFinalizar(LocalDateTime dataParaFinalizar) {
+		this.dataParaFinalizar = dataParaFinalizar;
+	}
+
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", dataParaFinalizar="
+				+ dataParaFinalizar + ", finalizado=" + finalizado + "]";
+	}
+
+}
